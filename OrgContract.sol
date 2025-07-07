516265005375058462b28caa935f891dcb2c472b
Shashank-765 <shashank@metaspacechain.com>
2025-07-07 13:25:35 +0530

    Payment implementation

--- OrgContract.sol
+++ OrgContract.sol
@@ -15,11 +15,17 @@ contract OrgContract {
         uint256 uploadedAt;
     }
 
-    mapping(uint256 => Document) public documents;
-    uint256 public docCount;
+    // user => their documents
+    mapping(address => Document[]) public documentsByUser;
 
-    event DocumentUploaded(uint256 docId, address indexed user, string cid, string docType);
-    event DocumentVerified(uint256 docId, Status status);
+    // unique list of users
+    address[] public users;
+
+    // used to track unique users
+    mapping(address => bool) public isUserKnown;
+
+    event DocumentUploaded(address indexed user, string cid, string docType);
+    event DocumentVerified(address indexed user, uint256 index, Status status);
 
     modifier onlyOrgAdmin() {
         require(msg.sender == orgAdmin, "Only Org Admin");
@@ -32,30 +38,59 @@ contract OrgContract {
     }
 
     function uploadDocument(string memory _cid, string memory _docType) external {
-        documents[docCount] = Document({
+        documentsByUser[msg.sender].push(Document({
             cid: _cid,
             docType: _docType,
             uploadedBy: msg.sender,
             status: Status.Pending,
             uploadedAt: block.timestamp
-        });
+        }));
 
-        emit DocumentUploaded(docCount, msg.sender, _cid, _docType);
-        docCount++;
+        if (!isUserKnown[msg.sender]) {
+            users.push(msg.sender);
+            isUserKnown[msg.sender] = true;
+        }
+
+        emit DocumentUploaded(msg.sender, _cid, _docType);
     }
 
-    function verifyDocument(uint256 _docId, bool approve) external onlyOrgAdmin {
-        require(_docId < docCount, "Invalid Doc ID");
-        documents[_docId].status = approve ? Status.Approved : Status.Rejected;
-        emit DocumentVerified(_docId, documents[_docId].status);
+    function verifyDocument(address user, uint256 index, bool approve) external onlyOrgAdmin {
+        require(index < documentsByUser[user].length, "Invalid index");
+        documentsByUser[user][index].status = approve ? Status.Approved : Status.Rejected;
+        emit DocumentVerified(user, index, documentsByUser[user][index].status);
     }
 
-    function getDocument(uint256 _docId) external view returns (Document memory) {
-        return documents[_docId];
+    function getDocument(address user, uint256 index) external view returns (Document memory) {
+        require(index < documentsByUser[user].length, "Invalid index");
+        return documentsByUser[user][index];
     }
 
-    function getDocumentCID(uint256 _docId) external view returns (string memory) {
-        require(documents[_docId].status == Status.Approved, "Not verified");
-        return documents[_docId].cid;
+    function getAllDocuments(address user) external view returns (Document[] memory) {
+        return documentsByUser[user];
+    }
+
+    function getAllUsers() external view returns (address[] memory) {
+        return users;
+    }
+
+    function getApprovedDocuments(address user) external view returns (Document[] memory) {
+        Document[] memory all = documentsByUser[user];
+        uint256 count;
+
+        for (uint256 i = 0; i < all.length; i++) {
+            if (all[i].status == Status.Approved) {
+                count++;
+            }
+        }
+
+        Document[] memory approved = new Document[](count);
+        uint256 j = 0;
+        for (uint256 i = 0; i < all.length; i++) {
+            if (all[i].status == Status.Approved) {
+                approved[j++] = all[i];
+            }
+        }
+
+        return approved;
     }
 }
--- superadmin.sol
+++ superadmin.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.8.0;
 
 import "./OrgContract.sol";
 
-
 contract SuperAdmin {
     address public owner;
 
     struct OrgInfo {
         address orgContractAddress;
         string orgName;
+        address orgAdmin;
     }
 
-    mapping(address => OrgInfo) public orgs;
-    address[] public allOrgs;
+    OrgInfo[] public allOrgs;
+    mapping(address => OrgInfo) public orgsByAdmin;
 
     event OrgCreated(address indexed orgAdmin, address contractAddress, string orgName);
+    event AdminFunded(address indexed orgAdmin, uint256 amount);
 
     constructor() {
         owner = msg.sender;
@@ -26,15 +27,62 @@ contract SuperAdmin {
         _;
     }
 
-    function createOrganization(address orgAdmin, string memory orgName) external onlyOwner {
-        require(orgAdmin  != address(0), "nota zero address");
+    receive() external payable {}
+
+    /// @notice Create new org and fund its admin with 0.1 MATIC
+    function createOrganization(address orgAdmin, string memory orgName) external payable onlyOwner {
+        require(orgAdmin != address(0), "Invalid admin address");
+        require(address(this).balance >= 0.1 ether, "Not enough balance to fund admin");
+
         OrgContract newOrg = new OrgContract(orgAdmin, orgName);
-        orgs[orgAdmin] = OrgInfo(address(newOrg), orgName);
-        allOrgs.push(address(newOrg));
+
+        OrgInfo memory info = OrgInfo({
+            orgContractAddress: address(newOrg),
+            orgName: orgName,
+            orgAdmin: orgAdmin
+        });
+
+        allOrgs.push(info);
+        orgsByAdmin[orgAdmin] = info;
+
+        // Send 0.1 MATIC to orgAdmin wallet
+        (bool success, ) = orgAdmin.call{value: 0.1 ether}("");
+        require(success, "Funding admin failed");
+
         emit OrgCreated(orgAdmin, address(newOrg), orgName);
+        emit AdminFunded(orgAdmin, 0.1 ether);
     }
 
-    function getAllOrgs() external view returns (address[] memory) {
+    /// @notice Return all org info
+    function getAllOrgs() external view returns (OrgInfo[] memory) {
         return allOrgs;
+    }
+
+    /// @notice Get MATIC balances of all org admins
+    function getAllOrgAdminBalances() external view returns (address[] memory admins, uint256[] memory balances) {
+        uint256 len = allOrgs.length;
+        admins = new address[](len);
+        balances = new uint256[](len);
+
+        for (uint256 i = 0; i < len; i++) {
+            admins[i] = allOrgs[i].orgAdmin;
+            balances[i] = allOrgs[i].orgAdmin.balance;
+        }
+    }
+
+    /// @notice Fund a specific org admin manually
+    function fundOrgAdmin(address payable orgAdmin, uint256 amount) external onlyOwner {
+        require(orgAdmin != address(0), "Invalid address");
+        require(address(this).balance >= amount, "Insufficient contract balance");
+
+        (bool sent, ) = orgAdmin.call{value: amount}("");
+        require(sent, "Transfer failed");
+
+        emit AdminFunded(orgAdmin, amount);
+    }
+
+    /// @notice Check contract balance
+    function getSuperAdminBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
